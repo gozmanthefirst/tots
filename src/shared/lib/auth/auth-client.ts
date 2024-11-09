d919b55b6dbe422c9f00f9a53bd27af49b89b92e
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-11-09 09:06:21 +0100

    fix dockerfile issue

--- src/shared/lib/auth/auth-client.ts
+++ src/shared/lib/auth/auth-client.ts
@@ -1,7 +1,6 @@
 // External Imports
 import { createAuthClient } from "better-auth/react";
 
-export const { user, session, signUp, signIn, signOut, useSession } =
-  createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL!,
-  });
+export const { signUp, signIn, signOut, useSession } = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL!,
+});
