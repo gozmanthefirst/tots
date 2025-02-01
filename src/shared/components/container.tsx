23c9597e50ce73d8a0a8d459a674ad817006237c
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-01 13:57:22 +0100

    update container component

--- src/shared/components/container.tsx
+++ src/shared/components/container.tsx
@@ -1,20 +1,19 @@
 // External Imports
-import { forwardRef, HTMLAttributes } from "react";
+import { HTMLAttributes, Ref } from "react";
 
 // Local Imports
 import { cn } from "../lib/utils/cn";
 
-export interface ContainerProps extends HTMLAttributes<HTMLDivElement> {}
+export interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+  ref?: Ref<HTMLDivElement>;
+}
 
-export const Container = forwardRef<HTMLDivElement, ContainerProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cn("container max-w-6xl", className)}
-        {...props}
-      />
-    );
-  },
-);
-Container.displayName = "Container";
+export const Container = ({ className, ref, ...props }: ContainerProps) => {
+  return (
+    <div
+      ref={ref}
+      className={cn("container max-w-6xl", className)}
+      {...props}
+    />
+  );
+};
