3433ae88ec0bf888aa394b880cd621fdb606d03e
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-19 19:07:09 +0100

    refactor: add overlay blur to drawers and modals

--- src/shared/components/drawer.tsx
+++ src/shared/components/drawer.tsx
@@ -22,7 +22,7 @@ const DrawerOverlay = forwardRef<
   <DrawerPrimitive.Overlay
     ref={ref}
     className={cn(
-      "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:animate-in data-[state=open]:fade-in-0 fixed inset-0 z-95 bg-black/50",
+      "fixed inset-0 z-95 bg-black/50 backdrop-blur-xs data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:animate-in data-[state=open]:fade-in-0",
       className,
     )}
     {...props}
