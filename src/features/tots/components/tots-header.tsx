903e25206f108a19d69744bd0f723a6b2ca95030
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-04-14 17:37:31 +0100

    feat: add user button to header

--- src/features/tots/components/tots-header.tsx
+++ src/features/tots/components/tots-header.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 import { AnimatePresence, motion } from "motion/react";
 import { RotatingLines } from "react-loader-spinner";
 
+import { getUser } from "@/shared/api/get-user";
 import { Button } from "@/shared/components/button";
 import { Container } from "@/shared/components/container";
 import { signOut } from "@/shared/lib/auth/auth-client";
 import { cn } from "@/shared/lib/utils/cn";
+import { runParallelAction } from "@/shared/lib/utils/parallel-server-action";
 
 const buttonCopy = {
   idle: "Sign Out",
-  loading: <RotatingLines visible width="16" strokeColor="#E5E999" />,
+  loading: <RotatingLines visible width="16" strokeColor="#000000" />,
   success: "Signed Out!",
   error: "Error",
 };
@@ -26,13 +29,22 @@ export const TotsHeader = () => {
     "idle" | "loading" | "success" | "error"
   >("idle");
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   const variants = {
     initial: { opacity: 0, y: -40 },
     visible: { opacity: 1, y: 0 },
     exit: { opacity: 0, y: 40 },
   };
 
-  const handleSignOut = async () => {
+  const { data: { data: user } = {} } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => runParallelAction(getUser()),
+  });
+
+  const handleSignOut = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
     try {
       await signOut({
         fetchOptions: {
@@ -80,7 +92,113 @@ export const TotsHeader = () => {
             </div>
           </Link>
 
-          <div className="pointer-events-auto">
+          <div
+            onClick={() => setDropdownOpen((p) => !p)}
+            className={cn(
+              "pointer-events-auto relative flex cursor-pointer items-center gap-2 font-medium text-neutral-300 transition-colors duration-200 lg:hover:text-brand-400",
+              dropdownOpen ? "text-brand-400" : "text-neutral-300",
+            )}
+          >
+            <div className="relative z-60 flex size-8 items-center justify-center overflow-hidden rounded-full">
+              <Image
+                src={user?.image || "/images/default-avatar.png"}
+                alt="Avatar"
+                fill
+                className="rounded-full"
+              />
+            </div>
+
+            <span className="relative z-60">{user?.username}</span>
+
+            <AnimatePresence>
+              {/* Overlay */}
+              {dropdownOpen ? (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.5,
+                    bounce: 0.2,
+                  }}
+                  className="fixed inset-0 z-50 cursor-auto bg-black/20 backdrop-blur-sm"
+                />
+              ) : null}
+            </AnimatePresence>
+
+            <AnimatePresence mode="popLayout" initial={false}>
+              {dropdownOpen ? (
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                    y: -10,
+                    scale: 0.95,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                    scale: 1,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    y: -10,
+                    scale: 0.95,
+                  }}
+                  transition={{
+                    type: "spring",
+                    bounce: 0.4,
+                    duration: 0.4,
+                  }}
+                  className="absolute top-[calc(100%_+_16px)] right-0 z-50 flex min-w-72 cursor-auto flex-col border border-brand-400/40 bg-brand-400/5 px-6 py-4 shadow-lg"
+                >
+                  <div className="flex items-center gap-2">
+                    <div className="relative z-60 flex size-10 items-center justify-center overflow-hidden rounded-full">
+                      <Image
+                        src={user?.image || "/images/default-avatar.png"}
+                        alt="Avatar"
+                        fill
+                        className="rounded-full"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-semibold text-brand-400">
+                        {user?.name}
+                      </p>
+                      <p className="text-sm text-neutral-300">{user?.email}</p>
+                    </div>
+                  </div>
+
+                  <Button
+                    variant={"brand"}
+                    size={"sm"}
+                    disabled={buttonState !== "idle"}
+                    onClick={handleSignOut}
+                    className={cn("relative mt-4 w-full overflow-hidden")}
+                  >
+                    <AnimatePresence mode="popLayout" initial={false}>
+                      <motion.div
+                        key={buttonState}
+                        transition={{
+                          type: "spring",
+                          bounce: 0,
+                          duration: 0.3,
+                        }}
+                        initial="initial"
+                        animate="visible"
+                        exit="exit"
+                        variants={variants}
+                      >
+                        {buttonCopy[buttonState]}
+                      </motion.div>
+                    </AnimatePresence>
+                  </Button>
+                </motion.div>
+              ) : null}
+            </AnimatePresence>
+          </div>
+
+          {/* <div className="pointer-events-auto">
             <Button
               variant={"secondary"}
               size={"sm"}
@@ -101,7 +219,7 @@ export const TotsHeader = () => {
                 </motion.div>
               </AnimatePresence>
             </Button>
-          </div>
+          </div> */}
         </div>
       </header>
     </Container>
